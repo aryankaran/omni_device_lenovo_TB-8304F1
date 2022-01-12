9799491b760823eaaefb8ff1ec0cffbc6ce33c2b
Aryan Karan <aryankaran28022004@gmail.com>
2022-01-12 19:30:06 +0530

    Revert "Sign final image by default as this isn't required"
    This reverts commit f9a2b04c9e5c74aec855cdf5d693e4dff2a8c3ff.

--- omni_TB_8304F1.mk
+++ omni_TB_8304F1.mk
@@ -9,7 +9,6 @@
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/verity.mk)
 
 # Inherit from TB-8304F1 device
 $(call inherit-product, device/lenovo/TB_8304F1/device.mk)
@@ -17,14 +16,6 @@ $(call inherit-product, device/lenovo/TB_8304F1/device.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Sign image for locked bootloader
-
-# ifneq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
-PRODUCT_SUPPORTS_BOOT_SIGNER := true
-PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
-# endif
-
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := TB_8304F1
