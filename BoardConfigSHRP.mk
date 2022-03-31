b3d63f161c8191acb3ff2fd77da9943bedcfd79a
Aryan Karan <aryankaran28022004@gmail.com>
2022-04-01 00:14:04 +0530

    Update my name
    Signed-off-by: Aryan Karan <aryankaran28022004@gmail.com>

--- BoardConfigSHRP.mk
+++ BoardConfigSHRP.mk
@@ -1,6 +1,6 @@
 SHRP_DEVICE_CODE := TB_8304F1
 SHRP_PATH := device/lenovo/$(SHRP_DEVICE_CODE)
-SHRP_MAINTAINER := "Aryan Karan"
+SHRP_MAINTAINER := aryankaran
 SHRP_REC_TYPE := Normal
 SHRP_OFFICIAL := true
 SHRP_DEVICE_TYPE := A_Only
